8f66075aeb06ec201662e1736b814578f44d82ac
gferguson520 <gferguson520@users.noreply.github.com>
2019-08-29 16:21:01 -0400

    Add project files.

---
+++ Ism 4300 My first app/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ism_4300_My_first_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
