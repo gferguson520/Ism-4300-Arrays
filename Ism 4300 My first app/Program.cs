3ad3bcecaca7f0ae7f45ec4e1112fa8586891e46
gferguson520 <gferguson520@users.noreply.github.com>
2019-08-29 16:24:51 -0400

    here to fix

--- Ism 4300 My first app/Program.cs
+++ Ism 4300 My first app/Program.cs
@@ -10,6 +10,9 @@ namespace Ism_4300_My_first_app
     {
         static void Main(string[] args)
         {
+            //My first app for write
+            Console.WriteLine("This is my first Consule App");
+            Console.WriteLine("I am Learning how to publish to Git Hub");
         }
     }
 }
